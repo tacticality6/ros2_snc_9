eac01f725a9100002ffb82f9f108d887ac475295
Michael Reece <s39477643@student.rmit.edu.au>
2025-04-15 11:49:49 +1000

    Began boilerplating all systems launch file

---
+++ launch/snc_all_systems.launch.py
@@ -0,0 +1,57 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    
+    # Path to the Explore Lite launch file
+    explore_lite_launch_file = os.path.join(
+        get_package_share_directory('explore_lite'), 'launch', 'explore.launch.py')
+    
+    return LaunchDescription([
+        # Include Explore Lite launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(explore_lite_launch_file)
+        ),
+
+        # Start State Management node
+        Node(
+            package='ros2_snc_9',
+            executable='state_node_executable',
+            name='state_server',
+            output='screen'
+        ),
+
+        # Start Nav Node
+        Node(
+            package='ros2_snc_9',
+            executable='nav_node_executable',
+            name='navigation_node',
+            output='screen'
+        ),
+
+        # Start Marker Detection Node
+        Node(
+            package='ros2_snc_9',
+            executable='hazard_detection_executable',
+            name='hazard_detection',
+            output='screen'
+        ),
+
+        # Start Position Tracking & Return home node
+        Node(
+            package='ros2_snc_9',
+            executable='position_tracking_node.py',
+            name='position_tracking',
+            parameters=[
+                {'tracking_interval': 0.5},
+                {'path_history_topic': 'robot_path_explore'},
+                {'return_path_topic': 'robot_path_return'},
+                {'base_frame': 'base_link'},
+                {'map_frame': 'map'} 
+            ]
+        ),
+    ])
