8dbe0a28307847ac0a4a99b253136872cb026b23
Michael Reece <s39477643@student.rmit.edu.au>
2025-04-21 12:13:49 +1000

    Updated position tracking launch

--- launch/position_tracking.launch.py
+++ launch/position_tracking.launch.py
@@ -3,16 +3,27 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Start Position Tracking & Return home node
         Node(
             package='ros2_snc_9',
-            executable='position_tracking_node.py',
+            executable='position_tracking_executable',
             name='position_tracking',
             parameters=[
-                {'tracking_interval': 2},
-                {'path_history_topic': 'robot_path_explore'},
-                {'return_path_topic': 'robot_path_return'},
+                {'tracking_interval': 2.0},
+                {'path_history_topic': 'path_explore'},
+                {'return_path_topic': 'path_return'},
                 {'base_frame': 'base_link'},
-                {'map_frame': 'map'} 
+                {'map_frame': 'map'},
+                {'follow_waypoints_action': 'follow_waypoints'},
+                {'path_history_max_length' : 1000} 
             ]
+        ),
+
+        # Start State Management node
+        Node(
+            package='ros2_snc_9',
+            executable='state_node_executable',
+            name='state_server',
+            output='screen'
         ),
     ])
